485d681a61c411dfce1d9c65a5bb3074c2e9ea4d
Eric Sosebee <esosebee114@gmail.com>
2020-05-08 19:41:37 -0400

    Initial Changes

---
+++ CSConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSConsoleApp
+{
+    class Program
+    {
+        static bool shouldContinue = true;
+
+        static int Main()
+        {
+            Console.WriteLine("Hello World!");
+
+            while (shouldContinue == true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "quit") shouldContinue = false;
+                else Console.WriteLine(input);
+            }
+
+            return 0;
+        }
+    }
+}
