6164924d5310766ce2aee34384557eecb14a9b48
Eric Sosebee <esosebee114@gmail.com>
2020-05-08 22:40:35 -0400

    initial round of code transfer

---
+++ CSConsoleApp/src/rooms/Library.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSConsoleApp.src.core.models.rooms
+{
+    class Library
+    {
+        #region Java code
+
+    //    //    private final Door[] doors = {new Door(RoomId.STUDY.getId(),
+    //    //            "\nStraight ahead in the distance, you see a wooden door.",
+    //    //            new String[]{"straight", "ahead", "forward", "forwards"})};
+
+    //    private static final int id = RoomId.LIBRARY.getId();
+    //    private static final String name = "Library";
+    //private final int neighbor = RoomId.STUDY.getId();
+    //    private SimpleMonster monster;
+    //    private final String description = RoomDescriptions.library;
+    //private final String firstSearchDescription =
+    //        RoomDescriptions.libraryFirstSearch;
+    //private boolean hasBeenSearched;
+
+
+
+    //    public Library()
+    //    {
+    //        this.hasBeenSearched = false;
+    //        this.addItemToItems(new Flashlight());
+    //        this.addItemToItems(new Knife());
+    //        //        this.items.add(eItem.MATCHES);
+    //        this.monster = new SimpleMonster("gremlin", 50, 5, "blade");
+    //        if (admin)
+    //        {
+    //            this.addItemToItems(new Excalibur());
+    //        }
+    //    }
+
+    //    /***********************
+    //     * Getters and setters *
+    //     ***********************/
+
+    //    public int getId()
+    //    {
+    //        return id;
+    //    }
+
+    //    public String getName()
+    //    {
+    //        return name;
+    //    }
+
+    //    public String getDescription()
+    //    {
+    //        String retVal = description;
+    //        //        for (Door door : doors) {
+    //        //            retVal += door.getDescription();
+    //        //        }
+    //        if (monster != null)
+    //        {
+    //            if (monster.isDead())
+    //            {
+    //                retVal += "\nThere is a dead " + monster.getName() + " here.";
+    //            }
+    //            else
+    //            {
+    //                retVal += "\nThere is " + monster.getNameLong() + " here.";
+    //            }
+    //        }
+    //        return retVal;
+    //    }
+
+    //    public SimpleMonster getMonster()
+    //    {
+    //        return this.monster;
+    //    }
+
+    //    /******************
+    //     * Search methods *
+    //     ******************/
+    //    private String search()
+    //    {
+    //        ArrayList<iItem> itemsInRoom = this.getItems();
+    //        if (itemsInRoom.isEmpty())
+    //        {
+    //            return "There are no items to be found here.";
+    //        }
+    //        if (!this.hasBeenSearched)
+    //        {
+    //            this.hasBeenSearched = true;
+    //            return Shared.appendDescriptionToItemsString(
+    //                    this.firstSearchDescription, itemsInRoom);
+    //        }
+
+    //        return Shared.appendDescriptionToItemsString(
+    //                    RoomDescriptions.defaultSearchDescription, itemsInRoom);
+    //    }
+
+    //    /******************
+    //     * Custom methods *
+    //     ******************/
+    //    public void performCustomMethods(
+    //            String[] inputs)
+    //    {
+    //        switch (inputs[0])
+    //        {
+    //            case "b":
+    //            case "browse":
+    //                LibraryBookshelfAdventure.browseLibrary();
+    //                break;
+    //            case "r":
+    //            case "read":
+    //                break;
+    //            case "s":
+    //            case "search":
+    //                output(this.search());
+    //                break;
+    //            default:
+    //                output(GameStrings.PerformCustomMethodsBadInput);
+    //        }
+    //    }
+
+    //    /*****************
+    //     *    MOVEMENT   *
+    //     *****************/
+    //    public int go(String direction)
+    //    {
+    //        switch (direction)
+    //        {
+    //            case "ahead":
+    //            case "forward":
+    //            case "forwards":
+    //            case "straight":
+    //                return this.neighbor;
+    //            default:
+    //                return -1;
+    //        }
+    //    }
+
+    //    /******************
+    //     *    Attacking   *
+    //     ******************/
+    //    //    public void attack() {
+    //    //        // First, check if there is a monster to fight
+    //    //        if (monster == null) {
+    //    //            output(GameStrings.NothingToAttackHereString);
+    //    //            return;
+    //    //        } else if (monster.isDead()) {
+    //    //            output("The " + monster.getName() + " is dead.");
+    //    //        } else {
+    //    //            // If a monster exists, attack it
+    //    //            Shared.attack(monster);
+    //    //            if (monster.isDead()) {
+    //    //                output("You have slain the " + monster.getName() + ".");
+    //    //            } else {
+    //    //                // If the monster is still alive, defend against its attack:
+    //    //                Shared.defend(monster);
+    //    //                if (Game.player.isDead()) {
+    //    //                    Game.state = false;
+    //    //                    output(Game.player.death);
+    //    //                }
+    //    //            }
+    //    //        }
+    //    //    }
+
+        #endregion
+    }
+}
---
+++ CSConsoleApp/src/titles/GameStrings.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSConsoleApp.src.titles
+{
+    class GameStrings
+    {
+        #region Java code
+
+        //        static final String title = "Game Strings";
+
+        //        private static final String bigWordsThe = ""
+        //            + " _______ _   _ _____ \n"
+        //            + "|__   __| | | | ____|\n"
+        //            + "   | |  | |_| | |___ \n"
+        //            + "   | |  |  _  |  ___|\n"
+        //            + "   | |  | | | | |___ \n"
+        //            + "   |_|  |_| |_|_____|";
+
+        //        private static final String bigWordsHouse = "" +
+        //            " _   _  ___  _   _  ____ _____ \n" +
+        //            "| | | |/ _ \\| | | |/ ___|  ___|\n" +
+        //            "| |_| | | | | | | | |__ | |___ \n" +
+        //            "|  _  | | | | | | |\\__ \\|  ___|\n" +
+        //            "| | | | |_| | |_| |___| | |___ \n" +
+        //            "|_| |_|\\___/ \\___/|____/|_____|";
+
+        //        private static final String bigWordsWith = "" +
+        //            " _      _ _ _____ _   _ \n" +
+        //            "| |    | | |_   _| | | |\n" +
+        //            "| |    | | | | | | |_| |\n" +
+        //            "| | __ | | | | | |  _  |\n" +
+        //            "| |/  \\| | | | | | | | |\n" +
+        //            " \\__/\\__/|_| |_| |_| |_|  ";
+
+        //        private static final String bigWordsOne = "" +
+        //            "  ___  _   _ _____ \n" +
+        //            " / _ \\| \\ | |  ___|\n" +
+        //            "| | | |  \\| | |___ \n" +
+        //            "| | | |     |  ___|\n" +
+        //            "| |_| | |\\  | |___ \n" +
+        //            " \\___/|_| \\_|_____|";
+
+        //        private static final String bigWordsRoom = "" +
+        //            " _____   ___   ___  _     _  \n" +
+        //            "|  __ \\ / _ \\ / _ \\| \\   / | \n" +
+        //            "| |__) | | | | | | |  \\_/  | \n" +
+        //            "|     /| | | | | | | |\\_/| | \n" +
+        //            "| |\\  \\| |_| | |_| | |   | | \n" +
+        //            "|_|  \\_\\\\___/ \\___/|_|   |_| ";
+
+        //        private static final String welcome = ""
+        //            + "      Welcome To\n"
+        //            + "The House With One Room";
+        //        private static final String house = "" +
+        //            "        _____/\\  \n" +
+        //            "       /     \\|  \n" +
+        //            "       |  _  ||   \n" +
+        //            "       |_|_|_||  ";
+        //        private static final String house2 = "" +
+        //            "     /\\____________/\\         \n" +
+        //            "     |    _____/\\   |   \n" +
+        //            "     |   /     \\|   |   \n" +
+        //            "     |   |  _  ||   |   \n" +
+        //            "     |___|_|_|_||___|  ";
+
+        //        public static String getTitle()
+        //        {
+        //            return title;
+        //        }
+
+        //        public static String getTitleInBigWords()
+        //        {
+        //            return bigWordsThe +
+        //                    "\n" + bigWordsHouse +
+        //                    "\n" + bigWordsWith +
+        //                    "\n" + bigWordsOne +
+        //                    "\n" + bigWordsRoom;
+        //        }
+
+        //        public static String getWelcome()
+        //        {
+        //            return welcome;
+        //        }
+
+        //        public static String getHouse()
+        //        {
+        //            return house;
+        //        }
+
+
+        //        /**********************
+        //         * Hard-coded strings *
+        //         **********************/
+        //        public static final String NotInInventory = ""
+        //            + "No item of that name is currently in your inventory.";
+        //        public static final String InvalidNoun = "Invalid noun.";
+        //        public static final String PerformCustomMethodsBadInput = "bad input; try again or use 'help' for help.";
+        //        public static final String NothingToAttackHereString = "There is nothing to attack here.";
+        //        public static final String GoInvalidDirection = "You cannot move in that direction from here.";
+        //        public static final String DoorIsLocked = "The door is locked.";
+
+        //        public static String getEOGUser()
+        //        {
+        //            return "You have ended the game.\n"
+        //                    + "Come back soon! \n"
+        //                    + "The House is waiting.";
+        //        }
+
+        //        public static final String HelpDialogue = "" +
+        //            "Example Commands: \n" +
+        //            "'attack' - engage in battle with a hostile entity sharing your room. " +
+        //            "Equipped items will be used to enhance the possible damage you can deal.\n" +
+        //            "'character' - generates a report on the stats, inventory, and equipped items of your player\n" +
+        //            "'describe' - get current room description\n" +
+        //            "'drop [item]' - put down an item from your inventory\n" +
+        //            "'equip [item]' - puts an item from your inventory into your hand so that it can be used\n" +
+        //            "'exit' - 'y' or 'n' to exit the game\n" +
+        //            "'go [direction]' - move into an adjacent room " +
+        //            "(example directions: left, right, forward, back, up, down)\n" +
+        //            "'inventory' - view the items that you have in your inventory\n" +
+        //            "'search' - look around in your current room for items that you can interact with\n" +
+        ////            "'stats [item]' - view the specified item's stats\n" +
+        //            "'take [item]' - pick up an item that you find in your room\n" +
+        //            "'unlock' - unlocks a locked door, if the necessary key is equipped " +
+        //            "(example usage: 'unlock left door')\n" +
+        //            "'view [item]' - get details on the specified item, if it is in your current inventory";
+
+        //        public static final String endGameWin = "" +
+        //            "You open the door and step out into brilliant sunlight.\n" +
+        //            "Congratulations! You have beaten the house!";
+
+        //    //    public static final String noStatsAvailable = "" +
+        //    //            "There are no available stats for this item.";
+
+        #endregion
+
+        const string title = "Game Strings";
+
+        private const string BigWordsThe = ""
+            + " _______ _   _ _____ \n"
+            + "|__   __| | | | ____|\n"
+            + "   | |  | |_| | |___ \n"
+            + "   | |  |  _  |  ___|\n"
+            + "   | |  | | | | |___ \n"
+            + "   |_|  |_| |_|_____|";
+
+        private const string BigWordsHouse = "" +
+            " _   _  ___  _   _  ____ _____ \n" +
+            "| | | |/ _ \\| | | |/ ___|  ___|\n" +
+            "| |_| | | | | | | | |__ | |___ \n" +
+            "|  _  | | | | | | |\\__ \\|  ___|\n" +
+            "| | | | |_| | |_| |___| | |___ \n" +
+            "|_| |_|\\___/ \\___/|____/|_____|";
+
+        private const string BigWordsWith = "" +
+            " _      _ _ _____ _   _ \n" +
+            "| |    | | |_   _| | | |\n" +
+            "| |    | | | | | | |_| |\n" +
+            "| | __ | | | | | |  _  |\n" +
+            "| |/  \\| | | | | | | | |\n" +
+            " \\__/\\__/|_| |_| |_| |_|  ";
+
+        private const string BigWordsOne = "" +
+            "  ___  _   _ _____ \n" +
+            " / _ \\| \\ | |  ___|\n" +
+            "| | | |  \\| | |___ \n" +
+            "| | | |     |  ___|\n" +
+            "| |_| | |\\  | |___ \n" +
+            " \\___/|_| \\_|_____|";
+
+        private const string BigWordsRoom = "" +
+            " _____   ___   ___  _     _  \n" +
+            "|  __ \\ / _ \\ / _ \\| \\   / | \n" +
+            "| |__) | | | | | | |  \\_/  | \n" +
+            "|     /| | | | | | | |\\_/| | \n" +
+            "| |\\  \\| |_| | |_| | |   | | \n" +
+            "|_|  \\_\\\\___/ \\___/|_|   |_| ";
+
+        private const string Welcome = "\tWelcome To\n The House With One Room";
+        private const string Welcome2 = @"       Welcome To
+ The House With One Room";
+
+        private const string House = "" +
+            "        _____/\\  \n" +
+            "       /     \\|  \n" +
+            "       |  _  ||   \n" +
+            "       |_|_|_||  ";
+
+        private const string House2 =
+@"         _____/\
+        /     \|
+        |  _  ||
+        |_|_|_||";
+
+        //private static final string house2 = "" +
+        //    "     /\\____________/\\         \n" +
+        //    "     |    _____/\\   |   \n" +
+        //    "     |   /     \\|   |   \n" +
+        //    "     |   |  _  ||   |   \n" +
+        //    "     |___|_|_|_||___|  ";
+
+        public static string GetTitle()
+        {
+            return title;
+        }
+
+        public static string GetTitleInBigWords()
+        {
+            return BigWordsThe +
+                    "\n" + BigWordsHouse +
+                    "\n" + BigWordsWith +
+                    "\n" + BigWordsOne +
+                    "\n" + BigWordsRoom;
+        }
+
+        public static string GetWelcome()
+        {
+            return Welcome2;
+        }
+
+        public static string GetHouse()
+        {
+            return House2;
+        }
+
+
+        /**********************
+         * Hard-coded strings *
+         **********************/
+        //        public static final String NotInInventory = ""
+        //            + "No item of that name is currently in your inventory.";
+        //        public static final String InvalidNoun = "Invalid noun.";
+        //        public static final String PerformCustomMethodsBadInput = "bad input; try again or use 'help' for help.";
+        //        public static final String NothingToAttackHereString = "There is nothing to attack here.";
+        //        public static final String GoInvalidDirection = "You cannot move in that direction from here.";
+        //        public static final String DoorIsLocked = "The door is locked.";
+
+        //        public static String GetEOGUser()
+        //        {
+        //            return "You have ended the game.\n"
+        //                    + "Come back soon! \n"
+        //                    + "The House is waiting.";
+        //        }
+
+        public const string HelpDialogue = "" +
+            "Example Commands: \n" +
+            "'attack' - engage in battle with a hostile entity sharing your room. " +
+            "Equipped items will be used to enhance the possible damage you can deal.\n" +
+            "'character' - generates a report on the stats, inventory, and equipped items of your player\n" +
+            "'describe' - get current room description\n" +
+            "'drop [item]' - put down an item from your inventory\n" +
+            "'equip [item]' - puts an item from your inventory into your hand so that it can be used\n" +
+            "'exit' - 'y' or 'n' to exit the game\n" +
+            "'go [direction]' - move into an adjacent room " +
+            "(example directions: left, right, forward, back, up, down)\n" +
+            "'inventory' - view the items that you have in your inventory\n" +
+            "'search' - look around in your current room for items that you can interact with\n" +
+//            "'stats [item]' - view the specified item's stats\n" +
+            "'take [item]' - pick up an item that you find in your room\n" +
+            "'unlock' - unlocks a locked door, if the necessary key is equipped " +
+            "(example usage: 'unlock left door')\n" +
+            "'view [item]' - get details on the specified item, if it is in your current inventory";
+
+        public const string EndGameWin = "" +
+            "You open the door and step out into brilliant sunlight.\n" +
+            "Congratulations! You have beaten the house!";
+
+//    //    public static final String NoStatsAvailable = "" +
+//    //            "There are no available stats for this item.";
+    }
+}
